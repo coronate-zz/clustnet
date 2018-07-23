4d41fc1b3b708f0258cdd96e428e7ac0f4606c71
coronate <acoronadn@gmail.com>
2018-07-22 19:38:44 -0500

    Se agrega algoritmo genetico con implementacion en paralelo y optimizacion de hiperparamtros del algoritmo genetico

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -1,150 +1,69 @@
 import pandas as pd 
 import numpy as np 
 import random 
+from pprint import pprint 
+import copy
+import pdb, traceback, sys
+import multiprocessing
+import random
+import time
 
-random.seed( 1010 )
+from utils_genetic import generate_genoma, parallel_solve, generate_population, look_solutions, \
+                          score_genetics, save_solutions, sort_population, population_summary, select_topn, \
+                          cross_mutate, score_test, solve_genetic_algorithm, report_genetic_results
 
-GENLONG = 100 
-def generate_genoma(GENLONG):
-    """
-    Generates a Random secuence of 1,0 of length GENLONG
-    """
-    genoma = ""
-    for i in range(GENLONG):
-        genoma += str(random.choice([1,0]))
-    return genoma
-
-
-def generate_population( N , GENLONG):
-    POPULATION_X = dict()
-    for i in range(N):
-        individual = generate_genoma(GENLONG)
-        POPULATION_X[individual] = {"SCORE": np.nan, "PROB": np.nan}
-        print("New individual generated: {}".format(individual))
-    return POPULATION_X
-
-def look_solutions(genoma, SOLUTIONS):
-    try:
-        return SOLUTIONS[genoma]
-    except Exception as e:
-        print("genoma is not save in SOLUTIONS")
-        return False
-
-def score_genoma(genoma, OBJECTIVE):
-    new_score = 0 
-    for bit_genoma, bit_objective in zip(genoma, OBJECTIVE):
-        if bit_objective == bit_genoma:
-            new_score +=1
-    return new_score
-
-def save_solutions(genoma, new_score, SOLUTIONS):
-    SOLUTIONS[genoma] = new_score
-
-def score_population(POPULATION_X, SOLUTIONS):
-    for genoma in POPULATION_X.keys():
-        if look_solutions(genoma, SOLUTIONS):
-            new_score = look_solutions
-            POPULATION_X[genoma] = new_score
-        else:
-            new_score = score_genoma(genoma, OBJECTIVE)
-            save_solutions(genoma, new_score, SOLUTIONS)
-            POPULATION_X[genoma] = new_score
-    return POPULATION_X
+#random.seed( 1010 )
 
 
 
+model_type = "test"
+GENETIC_OPTIMIZATION_SOLUTIONS = dict()
+
+
+
+GENLONG = 10
 FIRST_ITERATION = True
-N = 100
-PC =.1   #Crossover probability
-PM = .05 #Mutation probability
-SOLUTIONS = dict()
-OBJECTIVE = generate_genoma(GENLONG)
-POPULATION_X = generate_population( N, GENLONG)
-POPULATION_X = score_population(POPULATION_X, SOLUTIONS)
+N = 5
+PC =.5   #Crossover probability
+PM = .25 #Mutation probability
+MAX_ITERATIONS = 1000
+GENOMA_TEST = 50
+N_WORKERS = 16
+"""
+For a particular genetic algorithm with GENLONG = len_population and 
+"""
 
-def sort_population(POPULATION_X):
-    sorted_population = sorted(POPULATION_X.items(), key=lambda x: x[1], reverse = True)
-    result = list()
-    for i in sorted_population:
-        result.append( list(i) )
+MODELS_dict = {
+"genetic": 
+   {
+   "function": score_genetics,
+   "model_type": "genetic",
+   "params":
+       {
+       #-------------------------Must Sum GENLONG--------------------------------------------
+       "len_population": 4,  #<---- test GA of POPULATION 0 - (len_population)^2
+       "len_pc": 3,          #<-----test GA of Crossover probability 0-1 in steps of 1/len_pc
+       "len_pm": 3,          #<-----test GA of Mutation probability  0-1 in steps of 1/len_pm 
 
-    return result
+       #-------------------------Long of genetic algorithm to test ----------------------------
+       "len_genoma": GENOMA_TEST,
+       "n_workers": 4,
+       "max_iterations": 1000
+       }
+   },
 
-def sorted_to_dict(sorted_population):
-    POPULATION_X = dict()
-    for i in range(len(sorted_population)):
-        values = sorted_population[i] 
-        print(values)
-        POPULATION_X[values[0]] = values[1]
-    return POPULATION_X
+"test": 
+    {
+        "function": score_test,
+        "model_type":"test",
+        "params":
+        {
+        "n_workers": 4
+        }
+    }
+}
 
-POPULATION_X = sorted_to_dict(sorted_population)
+POPULATION_X, SOLUTIONS = solve_genetic_algorithm( GENLONG,  N, PC, PM, N_WORKERS, MAX_ITERATIONS, MODELS_dict["genetic"])
 
-sorted_population = sort_population(POPULATION_X)
-
-POPULATION_Y = sorted_population.copy()
-for j in range(int(N/2)):
-    pc = random.uniform(1,0)
-
-    #CROSSOVER
-    if pc < PC:
-        best = sorted_population[j][0]
-        worst = sorted_population[N -j][0]
-        startBest =  best
-        startWorst = worst
-
-        genoma_crossover = random.randint(0, GENLONG)
-        best_partA  = best[:genoma_crossover]
-        worst_partA = worst[:genoma_crossover]
-
-        best_partB  =  best[genoma_crossover:]
-        worst_partB = worst[genoma_crossover:]
-
-        new_best    = best_partA  + worst_partB
-        new_worst   = worst_partA + best_partB
-
-        endBest = new_best
-        endWorst =  new_worst
-
-
-        sorted_population[j] = new_best
-        sorted_population[N-j]  = new_worst
-
-        print("\n\nCrossover Performed on gen {}: \n\t StartBest: {} \n\t EndBest: {} \n\t StartWorst: {} \n\t EndWorst: {}".format(genoma_crossover, startBest, endBest, startWorst, endWorst))
-
-for j in range(N):
-    #MUTATION
-
-    pm = random.uniform(1,0)
-    mutation_gen = random.randint(0, GENLONG)
-
-    if pm < PM:
-        #PERFORM MUTATION
-
-        mutated_genoma = sorted_population[j][0]
-        start = mutated_genoma
-        if mutated_genoma[mutation_gen] =="1":
-            mutated_genoma = list(mutated_genoma)
-            mutated_genoma[mutation_gen] = "0"
-            mutated_genoma = "".join(mutated_genoma)
-        else:
-            mutated_genoma = list(mutated_genoma)
-            mutated_genoma[mutation_gen] = "1"
-            mutated_genoma = "".join(mutated_genoma)
-
-        end = mutated_genoma
-
-        sorted_population[j][0] = mutated_genoma
-
-        print("\nMutation Performed at gen {}: \n\t Start: {} \n\t End: {}".format(mutation_gen, start, end))
-
-#RESELECT
-POPULATION_Y
-POPULATION_X = sorted_to_dict(sorted_population)
-POPULATION_X = score_population(POPULATION_X, SOLUTIONS)
-
-
-
-
-
+report_genetic_results( POPULATION_X[0]["GENOMA"], MODELS_dict["genetic"])
 
